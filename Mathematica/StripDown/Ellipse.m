f3af89b289bcf50af6467563ce4cee9be00573b6
Jasper Shemilt <jaspershemilt@virginmedia.com>
2015-11-24 20:09:27 +0000

    stuff

---
+++ Mathematica/StripDown/Ellipse.m
@@ -0,0 +1,22 @@
+(* Mathematica Package *)
+(* Created by Mathematica Plugin for IntelliJ IDEA *)
+
+(* :Title: Ellipse *)
+(* :Context: Ellipse` *)
+(* :Author: jaspershemilt *)
+(* :Date: 2015-11-23 *)
+
+(* :Package Version: 0.1 *)
+(* :Mathematica Version: *)
+(* :Copyright: (c) 2015 jaspershemilt *)
+(* :Keywords: *)
+(* :Discussion: *)
+
+BeginPackage["Ellipse`"]
+(* Exported symbols added here with SymbolName::usage *)
+
+Begin["`Private`"]
+
+End[] (* `Private` *)
+
+EndPackage[]
